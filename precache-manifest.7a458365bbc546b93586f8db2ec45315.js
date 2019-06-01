46a5b688ca4a46ba818a6bbaa07d5e020fb62abc
xuxiaofei <xuxiaofei1.0@outlook.com>
2019-06-01 20:26:38 +0800

    Updates

---
+++ precache-manifest.7a458365bbc546b93586f8db2ec45315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7dda00785f427db9596",
+    "url": "/SpaceOdyssey/static/js/runtime~main.0dd06cbc.js"
+  },
+  {
+    "revision": "73484f1e4598b6f0f7e9",
+    "url": "/SpaceOdyssey/static/js/main.7005ffaa.chunk.js"
+  },
+  {
+    "revision": "42ddea628812c7a16ab9",
+    "url": "/SpaceOdyssey/static/js/2.7bb6014d.chunk.js"
+  },
+  {
+    "revision": "73484f1e4598b6f0f7e9",
+    "url": "/SpaceOdyssey/static/css/main.b1a1955f.chunk.css"
+  },
+  {
+    "revision": "4e6aa753733242c9a23270499f7f9cdf",
+    "url": "/SpaceOdyssey/index.html"
+  }
+];
